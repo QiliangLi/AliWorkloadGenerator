89db3a9f7834d5a6fe9a763a97f70017f9fec981
QiliangLi <799069561@qq.com>
2021-11-09 20:45:05 +0800

    update

--- DockerTraceAnalysis.py
+++ DockerTraceAnalysis.py
@@ -4,6 +4,7 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from pandas import DataFrame, Series
 
 
 def get_filelist(dir):
@@ -93,43 +94,86 @@ def getDetailedInfo(path):
 
 
 def plotCDF(path):
-    data = []
+    putRecord={}
+    putRecord['Size']=[]
+    getRecord={}
+    getRecord['Size']=[]
+
     data = pd.read_csv(path, header=None)
-    print(list(data))
+    data.columns=['Name','Type','Size']
+
+    # 将size转换成MB
+    data['Size']/=1024*1024
+    # print(data)
 
     # 获取数据的总数
-    denominator = len(data[2])
+    requestNum = len(data['Size'])
+    for index, row in data.iterrows():
+        if row['Type']=="PUT":
+            putRecord['Size'].append(row['Size'])
+        else:
+            getRecord['Size'].append(row['Size'])
+
+    putRequestNum=len(putRecord['Size'])
+    getRequestNum=len(getRecord['Size'])
+    putDf=DataFrame(putRecord)
+    getDf=DataFrame(getRecord)
+
     # 将数据转换为Series
-    Data = pd.Series(data[2])
+    Data = pd.Series(data['Size'])
+    putData=pd.Series(putDf['Size'])
+    getData=pd.Series(getDf['Size'])
+
     # 利用value_counts方法进行分组频数计算
     Fre = Data.value_counts()
+    putFre = putData.value_counts()
+    getFre = getData.value_counts()
+    # print(Fre)
+
     # 对获得的表格整体按照索引自小到大进行排序
     Fre_sort = Fre.sort_index(axis=0, ascending=True)
+    putFre_sort = putFre.sort_index(axis=0, ascending=True)
+    getFre_sort = getFre.sort_index(axis=0, ascending=True)
+    # print(Fre_sort)
 
     # 重置表格索引
     Fre_df = Fre_sort.reset_index()
-    # 将频数转换成概率
-    Fre_df[0] = Fre_df[0] / denominator
+    putFre_df = putFre_sort.reset_index()
+    getFre_df = getFre_sort.reset_index()
+
     # 将列表列索引重命名
-    Fre_df.columns = ['Rds', 'Fre']
+    Fre_df.columns = ['Size', 'Fre']
+    putFre_df.columns = ['Size', 'Fre']
+    getFre_df.columns = ['Size', 'Fre']
+    # print(Fre_df)
+    # print(type(Fre_df))
+
+    # 将频数转换成概率
+    Fre_df['Fre'] = Fre_df['Fre'] / requestNum
+    putFre_df['Fre'] = putFre_df['Fre'] / putRequestNum
+    getFre_df['Fre'] = getFre_df['Fre'] / getRequestNum
 
     # 利用cumsum函数进行概率的累加并按照顺序添加到表格中
     Fre_df['cumsum'] = np.cumsum(Fre_df['Fre'])
+    putFre_df['cumsum'] = np.cumsum(putFre_df['Fre'])
+    getFre_df['cumsum'] = np.cumsum(getFre_df['Fre'])
 
     # 创建画布
     plot = plt.figure()
     # 只有一张图，也可以多张
     ax1 = plot.add_subplot(1, 1, 1)
     # 按照Rds列为横坐标，累计概率分布为纵坐标作图
-    ax1.plot(Fre_df['Rds'], Fre_df['cumsum'])
+    ax1.plot(Fre_df['Size'], Fre_df['cumsum'])
+    ax1.plot(putFre_df['Size'], putFre_df['cumsum'])
+    ax1.plot(getFre_df['Size'], getFre_df['cumsum'])
     # 图的标题
     ax1.set_title("CDF")
     # 横轴名
-    ax1.set_xlabel("Rds")
+    ax1.set_xlabel("Object Size(MB)")
     # 纵轴名
     ax1.set_ylabel("P")
     # 横轴的界限
-    ax1.set_xlim(0.1, 0.5)
+    # ax1.set_xlim(0.1, 0.5)
     # 图片显示
     plt.show()
 
